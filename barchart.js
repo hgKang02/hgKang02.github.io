2de5c0ea9bf1479b9a5d0f25963852939329969d
hgKang02 <hangoo94@gmail.com>
2023-07-29 00:37:37 +0900

    .

--- barchart.js
+++ barchart.js
@@ -2,7 +2,7 @@ function main() {
     var svg = d3.select("svg"),
     margin = 200,
     width = svg.attr("width") - margin,
-    height = svg.attr("height") - margin,
+    height = svg.attr("height") - margin;
 
     var xScale = d3.scaleBand().range([0, width]).padding(0.4),
         yScale = d3.scaleLinear().range([height, 0]);
